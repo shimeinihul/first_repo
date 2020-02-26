c77ae405c7495cdedba51ec411d6b93d7e10ae18
shimeinihul <shimeinihul@gmail.com>
2020-02-26 17:35:35 +0900

    lesson04

--- mamazon/models.py
+++ mamazon/models.py
@@ -8,3 +8,6 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='thumbnail/')
+
+    def __str__(self):
+        return self.name
